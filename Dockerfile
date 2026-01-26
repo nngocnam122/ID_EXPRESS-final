2f244400e15e08f1e6c540f2dd75fe00b3a8923f
Nam Nguyen <nngocnam122@gmail.com>
2026-01-26 22:11:48 +0700

    Update l3

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,21 @@
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 WORKDIR /app
 
-COPY . .
+# Copy csproj & restore (cache layer)
+COPY *.csproj ./
 RUN dotnet restore
-RUN dotnet publish -c Release -o out
+
+# Copy all source
+COPY . ./
+
+# Publish project (not solution)
+RUN dotnet publish Renew_IDLine.csproj -c Release -o /app/out --no-restore
+
 
 # Runtime stage
 FROM mcr.microsoft.com/dotnet/aspnet:8.0
 WORKDIR /app
+
 COPY --from=build /app/out .
 
 ENV ASPNETCORE_URLS=http://+:10000
--- Models/Contact.cs
+++ Models/Contact.cs
@@ -2,11 +2,11 @@
 {
     public class ContactModel
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Subject { get; set; }
-        public string Message { get; set; }
+        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
     }
 }
